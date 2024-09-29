df73c7fa80b8b8d81f2fb3fb9f31d2030940c77b
Jaymin Rabari <114844115+jayminrabari@users.noreply.github.com>
2024-09-29 23:16:19 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,44 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.use(express.json());
+app.use(cors());
+
+const bookRoutes = require("./routes/bookRoutes");
+const userRoutes = require("./routes/userRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+
+app.use("/api/books", bookRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/transactions", transactionRoutes);
+
+// Connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw new Error("Failed to connect to MongoDB");
+  }
+};
 
 // Export the app for Vercel
 module.exports = async (req, res) => {
+  // Connect to MongoDB only when the function is invoked
+  if (!mongoose.connection.readyState) {
+    await connectDB();
+  }
+  
+  // Handle requests using the Express app
   return app(req, res);
 };
