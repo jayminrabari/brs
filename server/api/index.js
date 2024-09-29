7bd1af1126424dac74cebade7b0794bf64a8bc1b
Jaymin Rabari <114844115+jayminrabari@users.noreply.github.com>
2024-09-29 23:11:35 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/api/test", (req, res) => {
-  res.send("API is working!");
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
+// Export the app for Vercel
 module.exports = async (req, res) => {
   return app(req, res);
 };
